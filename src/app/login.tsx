f2f929d071ca32dc1da5bebc10f1d48ac29fdd7d
Vinh Nguyen <vinhnguyen@192.168.1.8>
2024-07-02 09:56:53 +0700

    blabla

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,4 +1,9 @@
 import style from './style/login.module.css'
 export default function Login() {
-    return <h1 className={style.text}>Login</h1>
+    return (
+      <div>
+        <h1 className={style.text}>cuong</h1>
+        <h2>bao</h2>
+      </div>
+    )
   }
