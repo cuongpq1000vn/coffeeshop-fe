38e4fb80cedce6117a1e532e0075ab44fdee2b53
baofrovn <baofromvn2007@gmail.com>
2024-07-06 22:44:42 +0700

    add code

--- src/app/category-list.tsx
+++ src/app/category-list.tsx
@@ -1,11 +1,86 @@
-import style from './style/category.module.css';
+"use client"
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import style from "./style/category.module.css";
+import { Alert,Box, colors, Tab, Tabs } from '@mui/material';
+import { Avatar } from '@mui/material'; 
+const columns: GridColDef[] = [
+  { field: 'image', headerName: 'Product', width: 120, renderCell: (params) => <Avatar alt={params.row.Name} src={params.value} className={style.image}/> },
+  { field: 'Name', headerName: 'Name', width: 500 },
+  { field: 'Description', headerName: 'Description', sortable:false,width: 550 },
+  { field: 'status', headerName: 'Status', width: 200,renderCell: ({ value }) => {
+    let statusColor;
+    switch (value) {
+      case 'Active':
+        statusColor = 'green';
+        break;
+      case 'Inactive':
+        statusColor = 'red';
+        break;
+      default:
+        statusColor = 'black'; 
+    }
+    return <span style={{ color: statusColor }}>{value}</span>;
+  },
+},
+];
+const rows = [
+  { id: 1, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description: "Very Hot Drinks", status: "Active" },
+  {id: 2, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description:"Very Hot Drinks", status:"Active" },
+  {id: 3, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description:"Very Hot Drinks", status:"Inactive"},
+  {id: 4, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description:"Very Hot Drinks", status:"Inactive"},
+  {id: 5, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description:"Very Hot Drinks", status:"Draft"},
+  {id: 6, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description:"Very Hot Drinks", status:"Active"},
+  {id: 7, image: "https://th.bing.com/th/id/OIP.iztlpqBdMWRs23FizYdZmAHaE7?rs=1&pid=ImgDetMain", Name: 'Coffee', Description:"Very Hot Drinks", status:"Active"},
+];
 
-export default function CategoryList() {
-    return (
-      <div>
-        <h1 className={style.text}>Category List</h1>
-        <h1> Bao </h1>
+export default function DataTable() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  const getRowHeight = (params) => {
+      // Adjust the logic here based on your needs
+      // You can access the row data using params.row
+    if (params.row.image.heig> 50) { // Example: Increase height for rows with long descriptions
+      return 80; // Set a desired height (in pixels)
+    } else {
+      return 60; // Default height (in pixels)
+    }
+    };
+  };
+  return (
+    <div className={style.all}>
+      <div className={style.head}>
+        <div className={style.column}><h1>Categories</h1></div>
+      
+        <div className={style.column}><button className={style.button}>+ Create Categories</button></div>
       </div>
-    )
-
-  }
+      <div className={style.bottom}>
+        <div className={style.box}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }} >
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="All Categories (20)"/>
+            <Tab label="Active (10)" />
+            <Tab label="Draft (5)" />
+            <Tab label="Inactive (5)" />
+            </Tabs>
+          </Box>
+        </div>
+      <div className={style.table}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 6},
+            },
+          }}
+          checkboxSelection
+        />
+      </div>
+        <Alert severity="info" className={style.alert}>Learn more about category</Alert>
+      </div>
+    </div>
+  );
+}
--- src/page/_app.tsx
+++ src/page/_app.tsx
@@ -1,5 +1,17 @@
-import type { AppProps } from 'next/app'
- 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import * as React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default MyApp;
