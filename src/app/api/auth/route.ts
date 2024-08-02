dc9f45015e6109a0114fd95603f4cfd9162efd2a
cuongpq1000vn <phamquoccuongv@gmail.com>
2024-08-02 09:07:55 +0700

    update code

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,7 +2,7 @@ import { TokenDTO } from "@/types/dtos/auth/Token";
 import { cookies } from "next/headers";
 export async function POST(request: Request) {
   const res = await request.json() as TokenDTO;
-  const sessionToken = JSON.stringify(res);
+  const sessionToken = res.token;
   if (!sessionToken) {
     return Response.json(
       { message: "Error token" },
